548b9e7debaa7b92ae5760498eb91b38d8c59b31
skilaru-godaddy <skilaru@godaddy.com>
2025-01-22 18:48:46 +0530

    renamed files and added Sessions Controller to handle login and logout

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  include Authentication
+
+  # Log in a user
+  def create
+    @user = User.find_by(email: params[:user][:email].downcase)
+
+    if user
+      if user.unconfirmed?
+        render json: { error: "Your account is not confirmed. Please check your email." }, status: :forbidden
+      elsif user.authenticate(params[:user][:password])
+        login(user) # Set session with the user's ID
+        render json: { message: "Login successful.", user: @user }, status: :ok
+      else
+        render json: { error: "Invalid email or password." }, status: :unauthorized
+      end
+    else
+      render json: { error: "Invalid email or password." }, status: :unauthorized
+    end
+  end
+
+  # Log out a user
+  def destroy
+    logout # Clear the session
+    render json: { message: "Logged out successfully." }, status: :ok
+  end
+end
