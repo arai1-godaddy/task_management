efc29fe177932e63bba5748a5603180faed4510e
arai1-godaddy <arai1@godaddy.com>
2025-01-27 19:31:06 +0530

    basic test done

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  subject(:task) { Task.new(title: "Sample Task", status: :pending, priority: :medium, user_id: 1) }
+  subject(:user) { User.create(name: "Saideep Kilaru", email: "test@example.com", password: "password", password_confirmation: "password") }
+  subject(:task) { Task.new(title: "Sample Task", status: 0, priority: 2, user_id: user.id) }
 
   describe 'validations' do
     it 'is valid with valid attributes' do
+      puts task.errors
       expect(task).to be_valid
     end
 
@@ -15,15 +17,13 @@ RSpec.describe Task, type: :model do
     end
 
     it 'is invalid with an invalid status' do
-      task.status = "invalid_status"
+      task.status = nil
       expect(task).to_not be_valid
-      expect(task.errors[:status]).to include("is not included in the list")
     end
 
     it 'is invalid with an invalid priority' do
-      task.priority = "invalid_priority"
+      task.priority = nil
       expect(task).to_not be_valid
-      expect(task.errors[:priority]).to include("is not included in the list")
     end
   end
 
@@ -34,13 +34,13 @@ RSpec.describe Task, type: :model do
     end
   end
 
-  # describe 'enums' do
-  #   it 'defines statuses enum correctly' do
-  #     expect(Task.statuses.keys).to contain_exactly('pending', 'in_progress', 'completed')
-  #   end
+  describe 'enums' do
+    it 'defines statuses enum correctly' do
+      expect(Task.statuses.keys).to contain_exactly('pending', 'in_progress', 'completed')
+    end
 
-  #   it 'defines priorities enum correctly' do
-  #     expect(Task.priorities.keys).to contain_exactly('low', 'medium', 'high')
-  #   end
-  # end
+    it 'defines priorities enum correctly' do
+      expect(Task.priorities.keys).to contain_exactly('low', 'medium', 'high')
+    end
+  end
 end
