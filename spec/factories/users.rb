f5ff4d46fac8644651fc8841e693ee622d0156df
skilaru-godaddy <skilaru@godaddy.com>
2025-01-27 12:32:55 +0530

    added factory for user to create one

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    name {"Saideep Kilaru"}
+    email {"deeps2657@gmail.com"}
+    password {"password"}
+    password_confirmation {"password"}
+  end
+end
